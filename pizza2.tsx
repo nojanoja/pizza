46b8fcf559c128112775157cbc0c574904533daf
nojanoja <75990903+nojanoja@users.noreply.github.com>
2021-05-08 17:38:44 +0200

    Add files via upload

---
+++ pizza2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="pizza2" tilewidth="32" tileheight="32" tilecount="361" columns="19">
+ <image source="pizza2.png" width="608" height="608"/>
+</tileset>
